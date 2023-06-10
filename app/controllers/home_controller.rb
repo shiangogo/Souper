5e6335179b6e08f7e140c52199dab8cabdde2172
Andrew Fei <andrew77498864@gmail.com>
2023-06-10 13:40:47 +0800

    Add line-bot-api and redis

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,53 @@
+require "line/bot"
+
 class HomeController < ApplicationController
+
   def index
     render plain: "Hello, world!"
   end
 
   def callback
+    p params
+    body = request.body.read
+    signature = request.env['HTTP_X_LINE_SIGNATURE']
+    unless client.validate_signature(body, signature)
+      return head :bad_request
+    end
+
+    events = client.parse_events_from(body)
+    events.each do |event|
+      p "*****************"
+      p event
+      p "*****************"
+      case event
+      when Line::Bot::Event::Message
+        case event.type
+        when Line::Bot::Event::MessageType::Text
+          message = {
+            type: 'text',
+            text: event.message['text']
+          }
+          client.reply_message(event['replyToken'], message)
+        when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
+          response = client.get_message_content(event.message['id'])
+          # tf = Tempfile.open("content")
+          # tf.write(response.body)
+          p response.body
+        end
+      end
+    end
     
+    render status: 200
   end
+
+  private
+
+  def client
+    @client ||= Line::Bot::Client.new { |config|
+      config.channel_id = ENV["LINE_CHANNEL_ID"]
+      config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+      config.channel_token = ENV["LINE_CHANNEL_ACCESS_TOKEN"]
+    }
+  end
+
 end
