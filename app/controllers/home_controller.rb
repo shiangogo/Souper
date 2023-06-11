7f9746d545dcba9f857d9f104b000eb6a68a32ca
Andrew Fei <andrew77498864@gmail.com>
2023-06-11 20:55:59 +0800

    Add end game prompt

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -30,9 +30,15 @@ class HomeController < ApplicationController
 
           elsif event.message["text"] == "湯師傅上湯" && $redis.get(event.chat_id)
             p "您有正在進行中的遊戲"
-            client.reply_message(event['replyToken'], {type: "text", text: "你有正在進行中的遊戲"})
+            client.reply_message(event['replyToken'], {type: "text", text: "你這道湯還沒喝完呢！"})
+            
+          elsif event.message["text"] == "喝完了" && $redis.get(event.chat_id)
+            $redis.del(event.chat_id)
+            client.reply_message(event['replyToken'], {type: "text", text: "好的，幫你清理清理～"})
+            
           elsif $redis.get(event.chat_id)
             client.reply_message(event['replyToken'], {type: "text", text: "功能還沒做好"})
+
           else
             p "啥都沒發生"
           end
