9a949e3db9ac08417133fbb36d2a7c8c984a655f
Andrew Fei <andrew77498864@gmail.com>
2023-06-13 16:17:27 +0800

    Add games_controller

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,42 @@
+class GamesController < ApplicationController
+  before_action :set_game, only: %i[show update destroy]
+  def index
+    @games = Game.all
+    render json: @games
+  end
+
+  def show
+    render json: @post
+  end
+
+  def create
+    @game = Game.new(game_params)
+    if @game.save
+      render json: @game, status: :created, location: @game
+    else
+      render json: @game.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @game.update(game_params)
+      render json: @game, status: :ok
+    else
+      render json: @game.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @game.destroy
+  end
+
+  private
+
+  def set_game
+    @game = Game.find(params[:id])
+  end
+
+  def game_params
+    params.require(:post).permit(:title, :description, :whole_story, :difficulty)
+  end
+end
