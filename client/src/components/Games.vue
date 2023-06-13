8e8013111d890d89d57db6baab597ee82c383256
Andrew Fei <andrew77498864@gmail.com>
2023-06-13 17:11:25 +0800

    Add Games component to get games in db

---
+++ client/src/components/Games.vue
@@ -0,0 +1,114 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <input
+      type="text"
+      class="title-input"
+      v-model="title"
+      placeholder="Title"
+    />
+    <input
+      type="text"
+      class="body-input"
+      v-model="description"
+      placeholder="Description"
+    />
+    <input
+      type="text"
+      class="body-input"
+      v-model="wholeStory"
+      placeholder="Whole story"
+    />
+    <label
+      >難度：<input type="number" v-model="difficulty" min="1" max="3"
+    /></label>
+    <button v-if="isEditing" @click="updateGame">Update</button>
+    <button v-if="isEditing" @click="cancelEdit">Cancel</button>
+    <button v-else @click="createGame">Create</button>
+
+    <div v-for="game in games" :key="game.id">
+      <div>
+        <h2>標題：{{ game.title }}</h2>
+        <p>故事片段：{{ game.description }}</p>
+        <p>完整故事：{{ game.whole_story }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// import { ref, onMounted } from "vue";
+export default {
+  name: "Games",
+  data() {
+    return {
+      games: [],
+      title: "Games",
+      description: "description",
+      wholeStory: "wholeStory",
+      difficulty: 0,
+      isEditing: false,
+      API_URL: "http://localhost:3000/games",
+    };
+  },
+  methods: {
+    async updateGame() {
+      return true;
+    },
+
+    cancelEdit() {
+      return true;
+    },
+    async createGame() {
+      const response = await fetch(API_URL, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          title: this.title,
+          description: this.description,
+          whole_story: this.wholeStory,
+        }),
+      });
+
+      const data = await response.json();
+      games.push(data);
+
+      this.title = "";
+      this.description = "";
+      this.wholeStory = "";
+    },
+  },
+  async mounted() {
+    const response = await fetch(this.API_URL);
+    this.games = await response.json();
+  },
+};
+</script>
+
+<style scoped>
+.title-input {
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  box-sizing: border-box;
+  bottom: -2px solid #ccc;
+  background-color: #f8f8f8;
+  color: #111;
+  border-radius: 4px;
+  resize: vertical;
+}
+
+.body-input {
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  box-sizing: border-box;
+  bottom: -2px solid #ccc;
+  background-color: #f8f8f8;
+  color: #111;
+  border-radius: 4px;
+  resize: vertical;
+}
+</style>
