fa1069c7b6c9d84897f18908413c95423384b15a
Andrew Fei <andrew77498864@gmail.com>
2023-06-14 22:34:47 +0800

    Add game edition feature

--- client/src/components/Games.vue
+++ client/src/components/Games.vue
@@ -22,17 +22,17 @@
     <label
       >難度：<input type="number" v-model="difficulty" min="1" max="3"
     /></label>
-    <button v-if="isEditing" @click="updateGame">Update</button>
+    <button v-if="isEditing" @click="updateGame(this.gameId)">Update</button>
     <button v-if="isEditing" @click="cancelEdit">Cancel</button>
     <button v-else @click="createGame">Create</button>
 
     <div v-for="game in games" :key="game.id">
       <div>
-        <h2>[{{ game.id }}] 標題： {{ game.title }}</h2>
+        <h2>標題： {{ game.title }}</h2>
         <p>故事片段：{{ game.description }}</p>
         <p>完整故事：{{ game.whole_story }}</p>
 
-        <button @click="updateGame(game.id)">Edit this game</button>
+        <button @click="editGame(game.id)">Edit this game</button>
         <button @click="deleteGame(game.id, game.title)">
           Delete this game
         </button>
@@ -47,6 +47,7 @@ export default {
   name: "Games",
   data() {
     return {
+      gameId: "",
       games: [],
       title: "",
       description: "",
@@ -57,12 +58,49 @@ export default {
     };
   },
   methods: {
-    async updateGame() {
-      return true;
+    async updateGame(id) {
+      const response = await fetch(`${this.API_URL}/${id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          title: this.title,
+          description: this.description,
+          whole_story: this.wholeStory,
+          difficulty: this.difficulty,
+        }),
+      });
+      const data = await response.json();
+      this.games.filter((game) => {
+        return game.id !== id;
+      });
+      this.games.push(data);
+      this.cancelEdit();
     },
 
     cancelEdit() {
-      return true;
+      this.isEditing = false;
+      this.title = "";
+      this.description = "";
+      this.wholeStory = "";
+      this.difficulty = 0;
+    },
+    async editGame(id) {
+      const game = this.games.find((game) => {
+        return game.id === id;
+      });
+      this.gameId = game.id;
+      this.title = game.title;
+      this.description = game.description;
+      this.wholeStory = game.whole_story;
+      this.difficulty = game.difficulty;
+      this.isEditing = true;
+
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
     },
     async deleteGame(id, title) {
       if (confirm(`你確定要刪除「${title}」嗎？`)) {
